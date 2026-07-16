3a72999e070c27f76741fa84c046437ebbd671d1
youknowimhigh <225402621+youknowimhigh@users.noreply.github.com>
2026-07-16 03:03:02 +0300

    Add messaging and history components

---
+++ Telegram/SourceFiles/core/version.h
@@ -0,0 +1,28 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#pragma once
+
+#include "base/const_string.h"
+
+#define TDESKTOP_REQUESTED_ALPHA_VERSION (0ULL)
+
+#ifdef TDESKTOP_ALLOW_CLOSED_ALPHA
+#define TDESKTOP_ALPHA_VERSION TDESKTOP_REQUESTED_ALPHA_VERSION
+#else // TDESKTOP_ALLOW_CLOSED_ALPHA
+#define TDESKTOP_ALPHA_VERSION (0ULL)
+#endif // TDESKTOP_ALLOW_CLOSED_ALPHA
+
+// used in Updater.cpp and Setup.iss for Windows
+constexpr auto AppId = "{B7D5D8DC-0362-424D-98C4-E202AE3F0EF5}"_cs;
+constexpr auto AppNameOld = "XGram for Windows"_cs;
+constexpr auto AppName = "XGram Desktop"_cs;
+constexpr auto AppFile = "XGram"_cs;
+constexpr auto AppVersion = 6009004;
+constexpr auto AppVersionStr = "6.9.4";
+constexpr auto AppBetaVersion = true;
+constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
